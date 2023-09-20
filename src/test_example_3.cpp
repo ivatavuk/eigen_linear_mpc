99e61ba4063bba8f8bd609e0a800bf08965770bd
ivatavuk <ivo.vatavuk@fer.hr>
2023-09-20 21:25:03 +0200

    Add initial test_example_3 including state constraints

---
+++ src/test_example_3.cpp
@@ -0,0 +1,168 @@
+#include "LinMpcEigen.hpp"
+#include "matplotlibcpp.hpp"
+#include "QpProblem.hpp"
+#include "ChronoCall.hpp"
+
+namespace plt = matplotlibcpp;
+
+Eigen::VectorXd generate_ramp_vec( uint32_t len, uint32_t ramp_half_period, double ramp_rate );
+std::vector<double> eigen2stdVec( Eigen::VectorXd eigen_vec );
+
+int main()
+{
+  // define ramp reference
+  static constexpr uint32_t n_simulate_steps = 1;
+  static constexpr uint32_t horizon = 200;
+  static constexpr double ramp_scaling = 0.1;
+  static constexpr double ramp_start = 1.0;
+  Eigen::VectorXd Y_d_full = generate_ramp_vec(horizon + n_simulate_steps, 15, 0.1) * ramp_scaling + ramp_start * Eigen::VectorXd::Ones(horizon + n_simulate_steps);
+
+  /**                   Define linear system
+   * x = [px, dpx]^T
+   * u = [ddpx]^T
+   * 
+   * y = [px]^T
+   */
+  static constexpr double T = 0.1;
+  Eigen::MatrixXd A(4, 4);
+  A <<  1, 0, T, 0,
+        0, 1, 0, T,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+
+  Eigen::MatrixXd B(4, 2);
+  B <<  T*T/2.0,  0,
+        0,        T*T/2.0,
+        T,        0,
+        0,        T;
+
+  Eigen::MatrixXd C(1, 4);
+  C <<  1, 1, 0, 0;
+  
+  Eigen::MatrixXd D = Eigen::MatrixXd::Zero(1, 2);
+
+  LinMpcEigen::LinearSystem example_system( A.sparseView(), 
+                                            B.sparseView(), 
+                                            C.sparseView(), 
+                                            D.sparseView());
+
+  
+  Eigen::VectorXd x0(4);
+  x0 << ramp_start, 0, 0, 0;
+  
+  VecNd Y_d = Y_d_full.segment(0, horizon);
+
+
+  static constexpr double inf = 9999.0;
+
+  VecNd u_lower_bound(2);
+  u_lower_bound << -0.3, -1.5;
+  VecNd u_upper_bound(2);
+  u_upper_bound << 0.3, 1.5;
+
+  VecNd x_lower_bound(4);
+  VecNd x_upper_bound(4);
+  x_lower_bound << -inf, -0.1, -inf, -inf;
+  x_upper_bound << inf, 0.1, inf, inf;
+
+  //MPC weights
+  /*
+  static constexpr double W_y = 8000;
+  static constexpr double wBddx = 10;
+  static constexpr double wAddx = 0;
+  static constexpr double wAx = 10;
+  */
+  static constexpr double W_y = 8000;
+  static constexpr double wBddx = 10;
+  static constexpr double wAddx = 0;
+  static constexpr double wAx = 10;
+
+  MatNd w_u(2, 2); 
+  w_u <<  wBddx,  0,
+          0,      wAddx;
+
+  MatNd w_x(4, 4);
+  w_x <<  0, 0,   0, 0,
+          0, wAx, 0, 0,
+          0, 0,   0, 0,
+          0, 0,   0, 0;
+
+  SparseMat w_u_sparse = w_u.sparseView();
+  SparseMat w_x_sparse = w_x.sparseView();
+
+  LinMpcEigen::MPC mpc(example_system, horizon, Y_d, x0, W_y, w_u_sparse, w_x_sparse, u_lower_bound, u_upper_bound, 
+                       x_lower_bound, x_upper_bound);
+  VecNd U_sol;
+  for(uint32_t i = 0; i < n_simulate_steps; i++)
+  {
+    if(i == 0)
+    {
+      std::cout << "First solver initialization:\n";
+      ChronoCall(microseconds,
+        mpc.initializeSolver();
+      );
+    }
+    else
+    { 
+      std::cout << "i = " << i << "\n";
+      Y_d = Y_d_full.segment(i, horizon);
+      x0 = mpc.calculateX(U_sol).segment(0, 2);
+      std::cout << "Updating MPC:\n";
+      ChronoCall(microseconds,
+        mpc.updateSolver(Y_d, x0);
+      );
+    }
+    
+    std::cout << "Solving:\n";
+    ChronoCall(microseconds,
+      U_sol = mpc.solve();
+    );
+
+    for(auto curr_U : mpc.extractU(U_sol))
+      plt::plot(curr_U);
+
+    plt::show();
+
+    std::vector<double> t_vector(Y_d.rows());
+    double temp_t = 0.0;
+    for(auto &t : t_vector)
+    {
+      t = temp_t;
+      temp_t += T;
+    }
+
+    auto x_vectors = mpc.extractX(U_sol);
+    for(uint32_t i = 0; i < 2; i++)
+      plt::plot(t_vector, x_vectors[i]);
+
+
+    plt::plot(t_vector, eigen2stdVec(Y_d));
+    plt::plot(t_vector, eigen2stdVec(mpc.calculateY(U_sol))); //Y does not show current point!!
+    //plt::plot(eigen2stdVec(Y_d));
+    //plt::plot(eigen2stdVec(mpc.calculateY(U_sol))); //Y does not show current point!!
+    plt::show();
+  }
+  
+  return 0;
+}
+
+Eigen::VectorXd generate_ramp_vec( uint32_t len, uint32_t ramp_half_period, double ramp_rate )
+{
+  Eigen::VectorXd lawnmower_vec(len);
+
+  lawnmower_vec(0) = 0.0;
+  for(uint32_t i = 1; i < len; i++)
+  {
+    lawnmower_vec(i) = !((i / ramp_half_period) % 2) ?  lawnmower_vec(i-1) : lawnmower_vec(i-1) + ramp_rate;
+  }
+
+  return lawnmower_vec;
+}
+
+std::vector<double> eigen2stdVec( Eigen::VectorXd eigen_vec )
+{
+  std::vector<double> ret_vec;
+  for(uint32_t i = 0; i < eigen_vec.rows(); i++)
+    ret_vec.push_back(eigen_vec(i));
+  return ret_vec;
+}
